eb804ed7ed83d60126a4302067b24d26bc875742
annaaborgogno <s311319@studenti.polito.it>
2025-06-26 10:47:59 +0200

    soluzione

--- accessGraph.py
+++ accessGraph.py
@@ -9,7 +9,11 @@ elist = [(1, 2, 1), (2, 3, 1), (1, 4, 1), (4, 2, 1),
 g.add_weighted_edges_from(elist)
 g.add_edge(2, 5, arbitraryAttr="foo")
 
-print(g[2])
+print(g[2]) #ottengo tutti i vicini del nodo 2, e per ognuno il peso dell'arco che li unisce
+# perché un grafo è un dizionario di dizionari di dizionari
+#{1: {'weight': 1}, 3: {'weight': 1}, 4: {'weight': 1}, 5: {'arbitraryAttr': 'foo'}}
+#le chiavi sono i nodi, e quindi g[2] contiene un dizionario dove le chiavi sono i nodi connessi al nodo 2
+# e i valori sono sono dizionari che definiscono il peso tra i due nodi 
 print("---------")
 print(g['a']['b'])
 print("---------")
@@ -18,7 +22,7 @@ print("---------")
 for n in g:
     print(n)
 print("---------")
-for nbr in g[2]:
+for nbr in g[2]: # di nuovo sto iterano su tutti i vicini del nodo 2
     print(nbr)
 print("---------")
 print(g[2][5]['arbitraryAttr'])
